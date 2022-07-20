cf3b6bb6c90cddda2b82239b6573c5191211f608
NoSegfault <development.my6565@gmail.com>
2022-07-20 00:32:05 -0400

    Changed USB task to core 0

--- usb.cc
+++ usb.cc
@@ -313,7 +313,7 @@ status StartUSBTask() {
   //                                 NULL, CONFIG_TASK_PRIORITY, &usb_task_handle);
   BaseType_t status = xTaskCreateAffinitySet(
       &USBTask, "usb_task", CONFIG_TASK_STACK_SIZE, NULL,
-      CONFIG_TASK_PRIORITY, (1 << 1), &usb_task_handle);
+      CONFIG_TASK_PRIORITY, (1 << 0), &usb_task_handle);
   if (status != pdPASS || usb_task_handle == NULL) {
     return ERROR;
   }
