79fb91c6d5e285df8910882b73e519501ea5fd49
NoSegfault <development.my6565@gmail.com>
2023-08-03 22:55:14 -0400

    Fixed key report frequency

--- linux/spi_module.c
+++ linux/spi_module.c
@@ -11,9 +11,31 @@
 
 #define INTERVAL_MS 10
 
+// Copied from drivers/hid/usbhid/usbkbd.c
+static const unsigned char usb_kbd_keycode[256] = {
+    0,   0,   0,   0,   30,  48,  46,  32,  18,  33,  34,  35,  23,  36,  37,
+    38,  50,  49,  24,  25,  16,  19,  31,  20,  22,  47,  17,  45,  21,  44,
+    2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  28,  1,   14,  15,  57,
+    12,  13,  26,  27,  43,  43,  39,  40,  41,  51,  52,  53,  58,  59,  60,
+    61,  62,  63,  64,  65,  66,  67,  68,  87,  88,  99,  70,  119, 110, 102,
+    104, 111, 107, 109, 106, 105, 108, 103, 69,  98,  55,  74,  78,  96,  79,
+    80,  81,  75,  76,  77,  71,  72,  73,  82,  83,  86,  127, 116, 117, 183,
+    184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 134, 138, 130, 132,
+    128, 129, 131, 137, 133, 135, 136, 113, 115, 114, 0,   0,   0,   121, 0,
+    89,  93,  124, 92,  94,  95,  0,   0,   0,   122, 123, 90,  91,  85,  0,
+    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   29,
+    42,  56,  125, 97,  54,  100, 126, 164, 166, 165, 163, 161, 115, 114, 113,
+    150, 158, 159, 128, 136, 177, 178, 176, 142, 152, 173, 140};
+
 static struct input_dev *button_dev;
 static struct timer_list timer;
 static struct spi_device *spi_device;
+
+static bool pressed;
 
 #define BUF_SIZE 256
 
@@ -122,6 +144,7 @@ static void spi_keyboard_fn(struct work_struct *work) {
     bytes += write_offset;
     curr_buf = buf + 1;
     consumed_bytes = 0;
+    bool has_key = false;
     do {
       memset(&segment, 0, sizeof(segment));
       bytes -= consumed_bytes;
@@ -130,13 +153,23 @@ static void spi_keyboard_fn(struct work_struct *work) {
       //        consumed_bytes);
       if (consumed_bytes > 0) {
         if (segment.field_type == IBP_KEYCODE) {
-          input_report_key(button_dev, KEY_1,
-                           segment.field_data.keycodes.keycodes[0] == 0x1e);
+          has_key = true;
+          if (pressed && segment.field_data.keycodes.keycodes[0] != 0x1e) {
+            pressed = false;
+            input_report_key(button_dev, KEY_1, 0);
+          } else if (!pressed &&
+                     segment.field_data.keycodes.keycodes[0] == 0x1e) {
+            input_report_key(button_dev, KEY_1, 1);
+            pressed = true;
+          }
           input_sync(button_dev);
         }
       } else {
-        input_report_key(button_dev, KEY_1, 0);
-        input_sync(button_dev);
+        if (!has_key && pressed) {
+          pressed = false;
+          input_report_key(button_dev, KEY_1, 0);
+          input_sync(button_dev);
+        }
       }
     } while (consumed_bytes > 0);
 
@@ -156,6 +189,7 @@ static void timer_callback(struct timer_list *data) {
 static int open(struct input_dev *dev) {
   timer_setup(&timer, timer_callback, 0);
   mod_timer(&timer, jiffies + msecs_to_jiffies(INTERVAL_MS));
+  pressed = false;
   return 0;
 }
 
@@ -252,9 +286,6 @@ static struct spi_driver picomk_spi_driver = {
     .shutdown = picomk_spi_shutdown,
 };
 module_spi_driver(picomk_spi_driver);
-
-// module_init(button_init);
-// module_exit(button_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("A");
